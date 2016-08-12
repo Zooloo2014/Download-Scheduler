47471c515e6fc1093f00d3d20cb5a3e7478d01a2
Brent <Zooloo2014@users.noreply.github.com>
2016-08-12 14:17:33 +0200

    Initial commit

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace NDM
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.bntExit = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listLog = new System.Windows.Forms.ListBox();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 335);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(635, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // bntExit
+            // 
+            this.bntExit.Location = new System.Drawing.Point(548, 294);
+            this.bntExit.Name = "bntExit";
+            this.bntExit.Size = new System.Drawing.Size(75, 23);
+            this.bntExit.TabIndex = 3;
+            this.bntExit.Text = "E&xit";
+            this.bntExit.UseVisualStyleBackColor = true;
+            this.bntExit.Click += new System.EventHandler(this.bntExit_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(548, 39);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(548, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listLog
+            // 
+            this.listLog.FormattingEnabled = true;
+            this.listLog.HorizontalScrollbar = true;
+            this.listLog.Location = new System.Drawing.Point(12, 10);
+            this.listLog.Name = "listLog";
+            this.listLog.ScrollAlwaysVisible = true;
+            this.listLog.Size = new System.Drawing.Size(530, 303);
+            this.listLog.TabIndex = 0;
+            this.listLog.TabStop = false;
+            // 
+            // textPort
+            // 
+            this.textPort.Location = new System.Drawing.Point(548, 91);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(75, 20);
+            this.textPort.TabIndex = 2;
+            this.textPort.Text = "9112";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 357);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.listLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.bntExit);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.Text = "Download Manager Service";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button bntExit;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listLog;
+        private System.Windows.Forms.TextBox textPort;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace NDM
+{
+    public partial class MainForm : Form
+    {
+        private AppSettings settings;
+        private TCPServer server;
+        private ListBoxLogger log;
+        private Scheduler scheduler;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            settings = new RegistryAppSettings("NDM");
+            settings.DefaultDirectory = @"C:\Users\bengelbr\Downloads";
+
+            log = new ListBoxLogger(listLog);
+            log.LogMessage("Application started");
+
+            server = new TCPServer((uint)Convert.ToInt32(textPort.Text));
+            server.OnLog += ServerLog;
+
+            log.LogMessage("Bind to port " + textPort.Text + " on localhost [" + getLocalIP() + "]");
+
+            scheduler = Scheduler.Instance;
+            scheduler.OnLog += ScheduleLog;
+            scheduler.DownloadDir = @"C:\Users\bengelbr\Downloads";
+            scheduler.Start();
+            log.LogMessage("Scheduler started");
+        }
+
+
+        /// <summary>
+        /// Determine localhost's IP address
+        /// </summary>
+        private string getLocalIP()
+        {
+            string r = "127.0.0.1";
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    r = ip.ToString();
+            }
+
+            return r;
+        }
+
+
+        /// <summary>
+        /// Event Handler for log messages from TCPServer and TCPServerClient
+        /// </summary>
+        public void ServerLog(TCPServer t, LogEventArgs e)
+        {
+            log.LogMessage(e.Text);
+        }
+
+
+        /// <summary>
+        /// Event Handler for log messages from Scheduler
+        /// </summary>
+        public void ScheduleLog(Scheduler t, LogEventArgs e)
+        {
+            log.LogMessage(e.Text);
+        }
+
+
+        private void bntExit_Click(object sender, EventArgs e)
+        {
+            log.LogMessage("Application ended");
+            Close();
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            log.LogMessage("Start listener");
+            server.Start();
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            log.LogMessage("Stop listener");
+            server.Stop();
+        }
+    }
+}
